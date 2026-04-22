7ee6437fb3e5910f1c95437fa4fa3cad2486edf3
Oleksandr Perehinchuk <paregeha@gmail.com>
2026-04-22 20:12:14 +0300

    fix gitignore

---
+++ lib/resources/app_themes.dart
@@ -0,0 +1,19 @@
+// import 'package:file_structure_flutter/resources/app_colors.dart';
+// import 'package:file_structure_flutter/resources/app_fonts.dart';
+// import 'package:flutter/material.dart';
+//
+// class AppThemes {
+//   const AppThemes._();
+//
+//   static ThemeData light() {
+//     //! Тут можно указать один шрифт на все страницы
+//     return ThemeData(
+//       fontFamily: AppFonts.fontFamily,
+//       scaffoldBackgroundColor: AppColors.scaffold,
+//       colorScheme: const ColorScheme.light().copyWith(
+//         primary: AppColors.primary,
+//         secondary: AppColors.secondary,
+//       ),
+//     );
+//   }
+// }
